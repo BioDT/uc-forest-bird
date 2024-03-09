eb367463ae57da03824fe366295528c51b93b2af
Samantha Wittke <samantha.wittke@csc.fi>
2024-03-09 11:43:54 +0200

    add resample script

--- scripts/merge.py
+++ scripts/merge.py
@@ -1,6 +1,5 @@
 '''
- Script to read LUKE volume and age files, assign all nodata values (32766) to one shared nodata value (32767) and merge all tiles in inputdirectory; takes inputdirectory and outputfilename
- '''
+ Script to read LUKE volume and age files, assign all nodata values (32766) to one shared nodata value (given as input) and merge all tiles in inputdirectory; takes inputdirectory, outputfilename and nodatavalue (32767 for age, 0 for volume) '''
 
 import sys
 import rasterio
@@ -9,6 +8,7 @@ import glob
 
 inputdir = sys.argv[1]
 outfile = sys.argv[2]
+nodatavalue = sys.argv[3]
 
 files = glob.glob(inputdir + '*.tif')
 
@@ -16,12 +16,15 @@ with rasterio.open(files[0]) as f:
     out_meta = f.meta.copy()
 
 arr, transform = rasterio.merge.merge(files)
+
 arr[arr == 32766] = 32767
+arr[arr == 32767] = nodatavalue
 
 out_meta.update({
     "height": arr.shape[1],
     "width": arr.shape[2],
     "transform": transform,
+    "nodata": nodatavalue
     })
 
 
