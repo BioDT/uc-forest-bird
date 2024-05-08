99eb49b3467031bc085d493abdf7366ee75b4ad0
Tuomas Rossi <tuomas.rossi@csc.fi>
2024-05-08 15:38:12 +0300

    Fix project directory in non-interactive mode

--- scripts/03_classify_rasters.R
+++ scripts/03_classify_rasters.R
@@ -12,19 +12,13 @@ if (!requireNamespace("terra", quietly = TRUE)) {
 
 library(terra)
 
-# Check if the script is being run from within RStudio
-if (interactive()) {
-  # Get the path of the currently running script
-  script_path <- file.path(dirname(rstudioapi::getActiveDocumentContext()$path), "03_classify_rasters.R")
-  
-  # Print the script path
-  print(script_path)
-} else {
-  print("Not running in interactive mode (e.g., not in RStudio).")
-}
-
 # Specify project directory
-project_directory <- dirname(dirname(script_path))
+if (interactive()) {
+  project_directory <- dirname(dirname(rstudioapi::getActiveDocumentContext()$path))
+} else {
+  project_directory <- getwd()
+}
+print(project_directory)
 
 # Specify input and output directories
 input_directory <- file.path(project_directory, "data", "resampled_rasters")
