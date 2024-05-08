0f3a8917d63ea13e64628b7c1d24d039d7702c07
Tuomas Rossi <tuomas.rossi@csc.fi>
2024-05-08 15:38:12 +0300

    Fix project directory in non-interactive mode

--- scripts/05_convert_m3_to_gm2.R
+++ scripts/05_convert_m3_to_gm2.R
@@ -10,19 +10,13 @@ if (!requireNamespace("terra", quietly = TRUE)) {
 
 library(terra)
 
-# Check if the script is being run from within RStudio
-if (interactive()) {
-  # Get the path of the currently running script
-  script_path <- file.path(dirname(rstudioapi::getActiveDocumentContext()$path), "05_convert_m3_to_gm2.R")
-  
-  # Print the script path
-  print(script_path)
-} else {
-  print("Not running in interactive mode (e.g., not in RStudio).")
-}
-
 # Specify project directory
-project_directory <- dirname(dirname(script_path))
+if (interactive()) {
+  project_directory <- dirname(dirname(rstudioapi::getActiveDocumentContext()$path))
+} else {
+  project_directory <- getwd()
+}
+print(project_directory)
 
 # Specify input and output directories
 input_directory <- file.path(project_directory, "data", "resampled_rasters")
