fa0b7ab5cec900c17f9bffa2a31e534adfdcc0b2
Tuomas Rossi <tuomas.rossi@csc.fi>
2025-02-20 12:20:29 +0200

    Check existence of input directories

--- scripts/submit_single.lumi.sh
+++ scripts/submit_single.lumi.sh
@@ -8,6 +8,7 @@
 #SBATCH --mem=8G
 #SBATCH -t 24:00:00
 
+# Parse command-line arguments
 ROOT_DIR="$1"
 if [ -z "$ROOT_DIR" ]; then echo "set directory"; exit 1; fi
 CLIMATE="$2"
@@ -15,15 +16,14 @@ if [ -z "$CLIMATE" ]; then echo "set climate scenario"; exit 1; fi
 HARVEST="$3"
 if [ -z "$HARVEST" ]; then echo "set harvest scenario"; exit 1; fi
 
+# Check input directories
 LANDIS_TEMPLATE_DIR="$ROOT_DIR/landis_template"
+if [ ! -d "$LANDIS_TEMPLATE_DIR" ]; then echo "directory missing: $LANDIS_TEMPLATE_DIR"; exit 1; fi
 SCENARIOS_DIR="$ROOT_DIR/scenarios"
-LANDIS_DIR="$ROOT_DIR/run_${SLURM_JOB_ID}/${CLIMATE}_${HARVEST}"
-SIF="$PWD/landis_0.3.1.sif"
-export SINGULARITY_BIND="/pfs,/scratch,/projappl,/project,/flash,/appl"
-
-time0=$(date +%s.%N)
+if [ ! -d "$SCENARIOS_DIR" ]; then echo "directory missing: $SCENARIOS_DIR"; exit 1; fi
 
 # Create new run directory
+LANDIS_DIR="$ROOT_DIR/run_${SLURM_JOB_ID}/${CLIMATE}_${HARVEST}"
 mkdir -p "$LANDIS_DIR"
 cp -p $LANDIS_TEMPLATE_DIR/* $LANDIS_DIR/
 cp "$SCENARIOS_DIR/climate_$CLIMATE.txt" $LANDIS_DIR/climate.txt
@@ -31,6 +31,8 @@ cp "$SCENARIOS_DIR/biomass_harvest_$HARVEST.txt" $LANDIS_DIR/biomass-harvest.txt
 time1=$(date +%s.%N)
 
 # Execute landis
+SIF="$PWD/landis_0.3.1.sif"
+export SINGULARITY_BIND="/pfs,/scratch,/projappl,/project,/flash,/appl"
 cd "$LANDIS_DIR"
 singularity run "$SIF" scenario.txt 2>&1 > out.txt
 time2=$(date +%s.%N)
